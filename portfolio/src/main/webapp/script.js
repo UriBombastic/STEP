c4a6c4415158c9452dff1a62aad1010382ba1b8a
UriHarding <UriHarding@google.com>
2020-06-11 21:26:47 +0000

    Got charts to work with hard-coded data

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -96,3 +96,27 @@ function cleanseString(html) {
   return html.replace(/</g, "&lt;").replace(/>/g, "&gt;");
 }
 
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(drawChart);
+
+function drawChart() {
+  const data = new google.visualization.DataTable();
+  data.addColumn('string', 'Animal');
+  data.addColumn('number', 'Count');
+        data.addRows([
+          ['Lions', 10],
+          ['Tigers', 5],
+          ['Bears', 15]
+        ]);
+
+  const options = {
+    'title': 'Zoo Animals',
+    'width':500,
+    'height':400
+  };
+
+  const chart = new google.visualization.PieChart(
+      document.getElementById('chart-container'));
+  chart.draw(data, options);
+}
+
